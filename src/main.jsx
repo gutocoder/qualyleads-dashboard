b1f9a7b2ba670fb3f69b66bf181d34560ee8393b
gutocoder <gustavoadade@gmail.com>
2026-03-23 16:51:57 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Onboarding from "./Onboarding.jsx";
+
+const path = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {path === "/onboarding" ? <Onboarding /> : <App />}
   </React.StrictMode>
 );
